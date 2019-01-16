8148f5bb34c902547dd8e4b4b3b99594a2243a3d
nairi <nairi.abgaryan@gmail.com>
2019-01-16 23:55:22 +0400

    [FEAT]:Config. Add mvc config for attribute routing

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -8,6 +8,7 @@ namespace CMovieWebApi
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 "Default",
                 "{controller}/{action}/{id}",
